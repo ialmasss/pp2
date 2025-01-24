c3ba5f7bfe1aec91f6e7da1e82fc5f932b110b8f
Almas <almass11dd@gmail.com~>
2025-01-25 03:48:32 +0500

    30

---
+++ variables/variables.py
@@ -0,0 +1,26 @@
+#1
+x = 5
+y = "Almas"
+print(x)
+print(y)
+
+#2
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#3
+x = 5
+y = "Almas"
+print(type(x))
+print(type(y))
+
+#4
+x = "Almas"
+# is the same as
+x = 'Almas'
+
+#5
+a = 4
+A = "kbtu"
+#A will not overwrite a
