ac647f8c00b4d9c873a5a9f5b85b432be959c9f7
Almas <almass11dd@gmail.com~>
2025-01-25 02:57:42 +0500

    2

--- lab1.py
+++ lab1.py
@@ -1,15 +1,21 @@
 #1
-x = int(1)   # x will be 1
-y = int(2.8) # y will be 2
-z = int("3") # z will be 3
+#This is a comment
+print("Hello, World!")
 
 #2
-x = float(1)     # x will be 1.0
-y = float(2.8)   # y will be 2.8
-z = float("3")   # z will be 3.0
-w = float("4.2") # w will be 4.2
-
+print("Hello, World!") #This is a comment
 #3
-x = str("s1") # x will be 's1'
-y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
+#print("Hello, World!")
+print("Cheers, Mate!")
+#4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+#5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
