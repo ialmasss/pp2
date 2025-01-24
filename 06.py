90d294d54719515734b161448638302747fbd780
Almas <almass11dd@gmail.com~>
2025-01-25 03:19:44 +0500

    6

---
+++ 06.py
@@ -0,0 +1,56 @@
+#syntax error
+#1
+if 5 > 2:
+    print("Five is greater than two!")
+#2
+if 5 > 2:
+    print("Five is greater than two!") 
+if 5 > 2:
+    print("Five is greater than two!") 
+    
+    #comments
+#3
+#print("Hello, World!")
+print("Cheers, Mate!")
+ 
+   #Variable
+   #4
+x = str(3)    
+y = int(3)    
+z = float(3)
+ #5
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#6
+
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#numbers
+#7
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#8
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#string
+
+#9
+a="qwerty1234"
+print(a)
+
+#10
+a = "Hello, World!"
+print(a.upper())
+#11
+a = "Hello, World!"
+print(a.lower())
