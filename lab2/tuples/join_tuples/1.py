1a7fc2f5afb16177cb63a65f881dc0e5985ed581
Almas <almass11dd@gmail.com~>
2025-01-28 16:57:07 +0500

    jt

---
+++ lab2/tuples/join_tuples/1.py
@@ -0,0 +1,16 @@
+#1
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = ("potato", "tomato", "carrot")
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#2
+tuple1 = (1,2,3)
+tuple1 = tuple1 * 2
+print(tuple1)
+
+#3
+tuple1 = ('a', 'b' , 'c')
+tuple2 = (1, 2, 3)
+tuple3 = tuple2 + tuple1
+print(tuple3)
