46fc107dbeb74082751dba4b254e79c2de245113
Almas <almass11dd@gmail.com~>
2025-01-28 17:19:19 +0500

    remove

---
+++ lab2/sets/remove_set/1.py
@@ -0,0 +1,14 @@
+#1
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+print(fruits)
+
+#2
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+print(fruits)
+
+#3
+fruits = {'apple', 'banana', 'cherry'}
+fruits.clear()
+print(fruits)
