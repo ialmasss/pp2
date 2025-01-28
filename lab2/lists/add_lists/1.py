779936cfe676db05a164b815f3ab4ee255f56bcc
Almas <almass11dd@gmail.com~>
2025-01-28 14:14:13 +0500

    a

---
+++ lab2/lists/add_lists/1.py
@@ -0,0 +1,17 @@
+#1
+mylist = ['apple', 'banana', 'cherry']
+mylist.insert(0, 'orange')
+print(mylist[1])
+
+#2
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#3
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+#4
+fruits = ['apple', 'banana', 'cherry']
+tropical = ['mango', 'pineapple', 'papaya']
+fruits.extend(tropical)
