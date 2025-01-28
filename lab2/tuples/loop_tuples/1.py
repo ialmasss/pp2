c64fb56733c2a41fe4f7f4e369aa1fb78b4af4f7
Almas <almass11dd@gmail.com~>
2025-01-28 16:52:13 +0500

    lp

---
+++ lab2/tuples/loop_tuples/1.py
@@ -0,0 +1,15 @@
+#1
+for x in ('apple', 'banana', 'cherry'):
+  print(x)
+
+#2
+mytuple = ('apple', 'banana', 'cherry')
+i = 0
+while i < len(mytuple):
+  print(mytuple[i])
+  i = i + 1
+
+#3
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])   
