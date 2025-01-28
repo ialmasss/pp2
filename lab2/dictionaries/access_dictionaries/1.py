55fd35e926bbec28270837d1ac1d10785b97421b
Almas <almass11dd@gmail.com~>
2025-01-28 18:25:11 +0500

    accdic

---
+++ lab2/dictionaries/access_dictionaries/1.py
@@ -0,0 +1,15 @@
+#1
+x = {'type' : 'fruit', 'name' : 'banana'}
+print(len(x))
+
+#2
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+#3
+x = {'type' : 'fruit', 'name' : 'banana'}
+print(x['type'])
