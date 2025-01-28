2fef80f8c369bb381b5ada22a33109280173157b
Almas <almass11dd@gmail.com~>
2025-01-28 17:14:22 +0500

    add

---
+++ lab2/sets/add_set_items/1.py
@@ -0,0 +1,10 @@
+#1
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+print(fruits)
+
+#2
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+print(fruits)
