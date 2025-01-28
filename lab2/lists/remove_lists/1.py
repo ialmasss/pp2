9c59b63f04456b0e95ddfc52434a0e6206b033c9
Almas <almass11dd@gmail.com~>
2025-01-28 14:21:29 +0500

    r

---
+++ lab2/lists/remove_lists/1.py
@@ -0,0 +1,13 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#2
+mylist = ['apple', 'banana', 'cherry']
+mylist.pop(1)
+print(mylist)
+
+#3
+fruits = ['apple', 'banana', 'cherry']
+fruits.clear()
+print(fruits)
