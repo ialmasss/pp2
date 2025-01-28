792c0070ab462d8e9f8ae3ea6563b939eb640fb6
Almas <almass11dd@gmail.com~>
2025-01-28 21:32:27 +0500

    copydic

---
+++ lab2/dictionaries/copy_dictionaries/1.py
@@ -0,0 +1,13 @@
+#1
+x = {'type' : 'fruit', 'name' : 'apple'}
+y = x.copy()
+print(y)
+
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
