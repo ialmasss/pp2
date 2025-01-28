87800fd76fa6133397450d8c9844c41767362f51
Almas <almass11dd@gmail.com~>
2025-01-28 21:12:46 +0500

    rmvdic

---
+++ lab2/dictionaries/remove_dictionary_items/1.py
@@ -0,0 +1,22 @@
+#1
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+print(car)
+
+#2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+print(car)
+
+#3
+myvar = {'type' : 'fruit', 'name' : 'apple', 'color' : 'red'}
+del myvar['color']
+print(myvar)
