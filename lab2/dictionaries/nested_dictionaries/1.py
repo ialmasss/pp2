9e201dec670ac48ca90353500f5499785507c94a
Almas <almass11dd@gmail.com~>
2025-01-28 22:38:24 +0500

    nestdic

---
+++ lab2/dictionaries/nested_dictionaries/1.py
@@ -0,0 +1,15 @@
+#1
+a = {'name' : 'John', 'age' : '20'}
+b = {'name' : 'May', 'age' : '23'}
+customers = {'c1' : a, 'c2' : b}
+print(customers['c2']['name'])
+
+#2
+a = {'name' : 'John', 'age' : 20}
+b = {'name' : 'May', 'age' : 23}
+customers = {'c1' : a, 'c2' : b}
+for x, obj in customers.items():  
+  print(x)
+    
+  for y in obj:
+    print(y + ':', obj[y])
