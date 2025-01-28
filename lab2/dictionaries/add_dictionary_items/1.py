2eeb46acf8d0681cca292f5914859d9c3c9e1f86
Almas <almass11dd@gmail.com~>
2025-01-28 19:00:59 +0500

    adddic

---
+++ lab2/dictionaries/add_dictionary_items/1.py
@@ -0,0 +1,13 @@
+#1
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+print(car)
+
+#2
+x = {'type' : 'fruit', 'name' : 'apple'}
+x.update({'color' : 'green'})
+print(x)
