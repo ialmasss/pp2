2394057278b1d0d1708277ef27797eadd3a05e68
Almas <almass11dd@gmail.com~>
2025-01-28 16:46:51 +0500

    up

---
+++ lab2/tuples/unpack_tuples/1.py
@@ -0,0 +1,16 @@
+#1
+fruits = ('apple', 'banana', 'cherry')
+(x, y, z) = fruits
+print(y)
+
+#2
+fruits = ('apple', 'banana', 'cherry')
+(x, *y) = fruits
+print(y)
+
+#3
+fruits = ('apple', 'banana', 'cherry', 'mango')
+(x, *y, z) = fruits
+print(y)
+
+#4
