d219bc32e684b17863e574ad9320e4f7a3015e51
Almas <almass11dd@gmail.com~>
2025-01-28 15:08:58 +0500

    l

---
+++ lab2/lists/list_comprehension/1.py
@@ -0,0 +1,13 @@
+#1
+fruits = ['apple', 'banana', 'cherry']
+newlist = [x for x in fruits if x == 'banana']
+print(newlist)
+
+#2
+fruits = ["apple", "banana", "cherry"]
+newlist = [x for x in fruits]
+
+#3
+fruits = ['apple', 'banana', 'cherry']
+newlist = ['apple' for x in fruits]
+print(newlist)
---
+++ lab2/lists/loop_lists/1.py
@@ -0,0 +1,13 @@
+#1
+for x in ['apple', 'banana', 'cherry']:
+  print(x)
+
+#2
+mylist = ['apple', 'banana', 'cherry']
+i = 0
+while i < len(mylist):
+  print(mylist[i])
+  i = i + 1
+    
+#3
+[print(x) for x in ['apple', 'banana', 'cherry']]
