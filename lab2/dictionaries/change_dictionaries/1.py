894f7436e92a8037f4432b58e9bc77ecd0751104
Almas <almass11dd@gmail.com~>
2025-01-28 18:54:00 +0500

    chngdic

---
+++ lab2/dictionaries/change_dictionaries/1.py
@@ -0,0 +1,18 @@
+#1
+x = {'type' : 'fruit', 'name' : 'banana'}
+x['type'] = 'berry'
+print(x)
+
+#2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car['year'] = 2020
+print(car)
+
+#3
+x = {'type' : 'fruit', 'name' : 'banana'}
+x.update({'name': 'apple'})
+print(x)
