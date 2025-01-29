a006dbc1a44df084f638a16e472d417a0369ad95
Almas <almass11dd@gmail.com~>
2025-01-29 14:48:15 +0500

    for

---
+++ lab2/for-loops/1.py
@@ -0,0 +1,28 @@
+#1
+for x in range(3):
+  print(x)
+
+#2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#3
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+#4
+for x in range(6):
+  print(x)
+
+#5
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+#6
