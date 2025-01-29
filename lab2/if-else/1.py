d1486e1a7d24bc98d85ee5325bcacdcf1211a5ba
Almas <almass11dd@gmail.com~>
2025-01-29 14:35:49 +0500

    ifelse

---
+++ lab2/if-else/1.py
@@ -0,0 +1,59 @@
+#1
+x = 5
+y = 8
+if x > y:
+  print('Hello')
+else:
+  print('Welcome')
+
+#2
+a = 50
+b = 10
+if a > b:
+  print("Hello World")
+
+#3
+a = 50
+b = 10
+if a != b:
+  print("Hello World")
+
+#4
+a = 50
+b = 10
+if a == b:
+  print("Yes")
+else:
+  print("No")
+
+#5
+a = 50
+b = 10
+if a == b:
+  print("1")
+elif a > b:
+  print("2")
+else:
+  print("3")
+
+#6
+a = 10
+b = 10
+c = 8
+d = 8
+if a == b and c == d:
+  print("Hello")
+
+#7
+a = 5
+if a == b or c == d:
+  print("Hello")
+
+#8
+if 5 > 2:
+  print("YES")
+
+#9
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
