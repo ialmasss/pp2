09d6019c0653e2ae1633f87fe9b225ebe650031e
Almas <almass11dd@gmail.com>
2025-02-14 23:18:48 +0500

    squ

--- lab4/generators/squares.py
+++ lab4/generators/squares.py
@@ -1,8 +1,8 @@
-l = int(input("Left bound: "))
-r = int(input("Right bound: "))
+a = int(input("Input A: "))
+b = int(input("Input B: "))
 
-def squares(l, r):
-    for i in range(l, r + 1):
+def squares(a, b):
+    for i in range(a, b + 1):
         yield i ** 2
 
-print(*squares(l, r), sep = ', ')
+print(*squares(a, b), sep = ', ')
